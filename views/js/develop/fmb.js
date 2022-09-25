49269e3e166fe27f861e2a932ae82d927853dfca
momus2000 <muhammadmustafa4000@gmail.com>
2022-09-25 19:27:30 -0400

    Client Side validation

--- views/js/develop/fmb.js
+++ views/js/develop/fmb.js
@@ -21,3 +21,31 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
     })
 })
+function validateUpload(input) {
+    const fileSize = input.files[0].size / 1024 / 1024; // in MiB
+    if (fileSize > 20) {
+      alert('File size exceeds 20 MiB');
+      clearFileInput("File")
+    }
+    const fileName = input.files[0].name.split('.').pop()
+    console.log(fileName)
+    if(fileName != "xlsx" && fileName != "xls"){
+        alert("Only excel files can be provided")
+        clearFileInput("File")
+    }
+}
+
+function clearFileInput(id) { 
+    var oldInput = document.getElementById(id); 
+    var newInput = document.createElement("input"); 
+
+    newInput.type = "file"; 
+    newInput.id = oldInput.id; 
+    newInput.name = oldInput.name; 
+    newInput.className = oldInput.className; 
+    newInput.style.cssText = oldInput.style.cssText; 
+    newInput.onchange = oldInput.onchange
+
+    oldInput.parentNode.replaceChild(newInput, oldInput); 
+}
+
