a97fc2b769d7b4053c4e08be12537c0962a07f5d
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2022-10-11 21:17:42 -0400

    Adding privacy policy

--- sms.mmuhammad.net/controllers/home/landing.go
+++ sms.mmuhammad.net/controllers/home/landing.go
@@ -8,12 +8,14 @@ import (
 )
 
 type Landing struct {
-	LandingPage *views.View
+	LandingPage   *views.View
+	PrivacyPolicy *views.View
 }
 
 func NewLandingPageController() *Landing {
 	return &Landing{
-		LandingPage: views.NewView("landingPage", "/home/landingPageContent.gohtml"),
+		LandingPage:   views.NewView("landingPage", "/home/landingPageContent.gohtml"),
+		PrivacyPolicy: views.NewView("privacy", "/home/privacy.gohtml"),
 	}
 }
 
@@ -21,8 +23,13 @@ func (landing *Landing) GetLandingPage(w http.ResponseWriter, r *http.Request) {
 	landing.LandingPage.Render(w, nil)
 }
 
+func (landing *Landing) GetPrivacyPolicyPage(w http.ResponseWriter, r *http.Request) {
+	landing.PrivacyPolicy.Render(w, nil)
+}
+
 func AddHomePageRoutes(r *mux.Router, landC *Landing) {
 	r.HandleFunc("/", landC.GetLandingPage).Methods("GET")
+	r.HandleFunc("/policy", landC.GetPrivacyPolicyPage).Methods("GET")
 	// Linking css files
 	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/",
 		http.FileServer(http.Dir("views/layout/style/"))))
--- sms.mmuhammad.net/views/js/landingPage/landingPage.js
+++ sms.mmuhammad.net/views/js/landingPage/landingPage.js
@@ -1,1 +1,4 @@
-// alert("hello")
+var anchors = document.getElementsByTagName('a');
+ for(i=0, len=anchors.length; i<len; i++){
+     anchors[i].addEventListener('click', function(e){e.preventDefault();});
+ }
