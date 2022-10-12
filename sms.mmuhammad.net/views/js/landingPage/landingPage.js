34c39964cfe667954ee9f9ded267d0d16873065a
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2022-10-11 21:28:25 -0400

    Fix ugly navlinks

--- sms.mmuhammad.net/views/js/landingPage/landingPage.js
+++ sms.mmuhammad.net/views/js/landingPage/landingPage.js
@@ -1,4 +1,15 @@
-var anchors = document.getElementsByTagName('a');
- for(i=0, len=anchors.length; i<len; i++){
-     anchors[i].addEventListener('click', function(e){e.preventDefault();});
- }
+// Like $('a'), gets all the <a> elements in the document.
+var aElements = document.getElementsByTagName('a');
+console.log(aElements)
+// Create one function object instead of one per <a> element.
+// The calling convention is the same for jQuery as for regular JS.
+function preventDefaultListener(e) { 
+    elem = e.srcElement.href.split("#")[1]
+    document.getElementById(elem).scrollIntoView()
+    e.preventDefault(); 
+}
+// For each a element,
+for (var i = 0, n = aElements.length; i < n; ++i) {
+  // register the listener to be fired on click.
+  aElements[i].addEventListener('click', preventDefaultListener);
+}
